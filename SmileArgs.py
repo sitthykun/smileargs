8e1503490629f4d47beaeb15e771325cbd27fc2c
Sitthykun LY <ly.sitthykun@gmail.com>
2023-07-20 03:58:42 +0700

    update

--- SmileArgs.py
+++ SmileArgs.py
@@ -1,4 +1,5 @@
 # built-in
+import re
 import sys
 
 
@@ -16,13 +17,14 @@ class SmileArgs:
 		NUM         = 'n'
 		PRIORITY    = 'p'
 		SHORT       = 's'
+		VALUE       = 'v'
 
 	class OptionPrefixSymbol:
 		"""
 
 		"""
-		SHORT   = '-'
 		LONG    = '--'
+		SHORT   = '-'
 
 	class OptionDelimiterSymbol:
 		"""
@@ -45,14 +47,99 @@ class SmileArgs:
 
 		"""
 		# private
-		self.__commandAssignedSymbol= ''
+		self.__commandAssignedSymbol= self.OptionDelimiterSymbol.EQUAL
 		self.__commandFound         = []
 		self.__commandList          = []
 		self.__commandMinNum        = 2
-		## commands
+		## command
 		self.__args                 = []
+		## value
+		self.__number               = 0
+		## pattern
+		self.__patternBase          = '([(\-\-)(\-)]+\w+)(=?(\w+)?)'
 
-	def __isLongCommandOption(self, command: str) -> bool:
+	def __addCollection(self, arg: str) -> None:
+		"""
+
+		:param arg:
+		:return:
+		"""
+		if self.__isValidValue(arg):
+			#
+			result = re.match(self.__patternBase, arg)
+			# command value
+			# result[0] result[2]
+			if self.__isLong(result[0]):
+				#
+				self.__args.append({
+					self.Key.LONG   : result[0]
+					, self.Key.SHORT: ''
+					, self.Key.VALUE: result[2]
+				})
+
+			else:
+				#
+				self.__args.append({
+					self.Key.LONG   : ''
+					, self.Key.SHORT: result[0]
+					, self.Key.VALUE: result[2]
+				})
+
+		else:
+			result = re.match(self.__patternBase, arg)
+			# result[0]
+			if self.__isLong(result[0]):
+				#
+				self.__args.append({
+					self.Key.LONG   : result[0]
+					, self.Key.SHORT: ''
+					, self.Key.VALUE: ''
+				})
+
+			else:
+				#
+				self.__args.append({
+					self.Key.LONG   : ''
+					, self.Key.SHORT: result[0]
+					, self.Key.VALUE: ''
+				})
+
+	def __cleanArg(self) -> list:
+		"""
+
+		:return:
+		"""
+		#
+		validList   = []
+
+		# scan valid list
+		# start from argv
+		for t in sys.argv[1:]:
+			#
+			if t.split(self.__commandAssignedSymbol):
+				pass
+
+		#
+		return validList
+
+	def __hasNoValue(self, command: str) -> bool:
+		"""
+
+		:param command:
+		:return:
+		"""
+		try:
+			# re.match('([(\-\-)(\-)]+\w+)(=?(\w+)?)', '-e')
+			# re.match('([(\-\-)(\-)]+\w+)(=?(\w+)?)', '--engine')
+			result  = re.match(self.__patternBase, command)
+
+			# ('--engine', '', None)
+			return (result.groups()[0]) and (result.groups()[1] == '' and result.groups()[2] is None)
+
+		except Exception as e:
+			return False
+
+	def __isLongCommand(self, command: str) -> bool:
 		"""
 
 		:param command:
@@ -76,7 +163,7 @@ class SmileArgs:
 		#
 		return found
 
-	def __isShortCommandOption(self, command: str) -> bool:
+	def __isShortCommand(self, command: str) -> bool:
 		"""
 
 		:param command:
@@ -104,33 +191,69 @@ class SmileArgs:
 		#
 		return found
 
-	def __isValidCommand(self, command: str) -> bool:
+	def __isLong(self, command: str) -> bool:
 		"""
 
 		:param command:
 		:return:
 		"""
-		# valid
-		isValid = False
+		return command.find(self.OptionPrefixSymbol.LONG) != -1
 
-		# check short and long
-		if self.__isShortCommandOption(command) or self.__isLongCommandOption(command):
-			#
-			pass
+	def __isShort(self, command: str) -> bool:
+		"""
 
-		#
-		return isValid
+		:param command:
+		:return:
+		"""
+		return  command.find(self.OptionPrefixSymbol.SHORT) != -1
 
-	def addElement(self, shortCommand: str, longCommand: str, description: str, num: int= 0, priority: int= Priority.IGNORE) -> None:
+	def __isValidCommand(self, args: str) -> bool:
+		"""
+
+		:param args:
+		:return:
+		"""
+		try:
+			# re.match('([(\-\-)(\-)]+\w+)(=?(\w+)?)', '-e')
+			# re.match('([(\-\-)(\-)]+\w+)(=?(\w+)?)', '--engine')
+			result = re.match(self.__patternBase, args)
+
+			# --engine => ('--engine', '', None)
+			return (result.groups()[0]) and (result.groups()[1] == '' and result.groups()[2] is None)
+
+		except Exception as e:
+			return False
+
+	def __isValidValue(self, args: str) -> bool:
+		"""
+
+		:param args:
+		:return:
+		"""
+		try:
+			# re.match('([(\-\-)(\-)]+\w+)(=?(\w+)?)', '-e')
+			# re.match('([(\-\-)(\-)]+\w+)(=?(\w+)?)', '--engine')
+			result = re.match(self.__patternBase, args)
+
+			# --engine=dsffads => ('--engine', '=dsffads', 'dsffads')
+			return (result.groups()[0] and result.groups()[1] and result.groups()[2])
+
+		except Exception as e:
+			return False
+
+	def addCommand(self, shortCommand: str, longCommand: str, description: str, priority: int= Priority.IGNORE, overrideNum: list= []) -> None:
 		"""
 
 		:param shortCommand:
 		:param longCommand:
 		:param description:
-		:param num:
 		:param priority:
+		:param overrideNum:
 		:return:
 		"""
+		# increase power by 2
+		self.__number   *= 2
+
 		#
 		self.__commandList.append(
 			{
@@ -141,65 +264,28 @@ class SmileArgs:
 				, self.Key.DESCRIPTION  : description
 				, self.Key.PRIORITY     : priority
 				# special value
-				, self.Key.NUM          : num
+				, self.Key.NUM          : self.__number
 			}
 		)
 
-	def findElement(self, command: str= '') -> bool:
+	def findCommand(self, args: str= '') -> bool:
 		"""
 		@search command
-		:param command:
+		:param args:
 		:return:
 		"""
-		#
-		found       = False
 		# command
+		commandKey  = ''
 		commandLong = ''
 		commandShort= ''
-		commandKey  = ''
+		#
+		found       = False
 
-		# check short or long
-		if self.__isShortCommandOption(command):
-			# loop to check command
-			for c in self.__commandList:
-				# command with value
-				if c.get(self.Key.SHORT) == command.split(self.__commandAssignedSymbol)[0]:
-					# .s == a, .l == alphabet
-					found   = True
-
-		elif self.__isLongCommandOption(command):
-			# loop to check command
-			for c in self.__commandList:
-				# command with value
-				if c.get(self.Key.SHORT) == command.split(self.__commandAssignedSymbol)[0]:
-					# .s == a, .l == alphabet
-					found = True
-
+		# ([(\-\-)(\-)]+\w+)(=?(\w+)?)
 		# after filter and calculation
 		return found
 
-		# #
-		# try:
-		# 	# loop list to find item
-		# 	for index in range(len(self.__commandList)):
-		# 		#
-		# 		for key in self.__commandList[index]:
-		# 			# find short command by each element
-		# 			if self.__commandList[index]:
-		# 				# del element
-		# 				self.__commandFound.append(shortCommand)
-		#
-		# 			elif longCommand and self.__commandList[index].get(self.Key.LONG) == longCommand:
-		# 				self.__commandFound.append(shortCommand)
-		#
-		# except Exception as e:
-		# 	print(f'{str(e)}')
-		# 	found   = False
-		#
-		# #
-		# return found
-
-	def getElement(self) -> list:
+	def getCommand(self) -> list:
 		"""
 
 		:return:
@@ -217,14 +303,14 @@ class SmileArgs:
 		#
 		return temp
 
-	def listElement(self) -> None:
+	def listCommand(self) -> None:
 		"""
 
 		:return:
 		"""
 		pass
 
-	def removeElement(self, shortCommand: str, longCommand: str= '') -> None:
+	def removeCommand(self, shortCommand: str, longCommand: str= '') -> None:
 		"""
 
 		:param shortCommand:
@@ -244,6 +330,16 @@ class SmileArgs:
 
 		except Exception as e:
 			print(f'{str(e)}')
+
+	def run(self) -> None:
+		"""
+
+		:return:
+		"""
+		#
+		for a in sys.argv[1:]:
+			if self.__isValidCommand(a):
+				self.__addCollection(a)
 
 	def setAssignSymbol(self, symbol: str= OptionDelimiterSymbol.EQUAL) -> None:
 		"""
---
+++ run.py
@@ -0,0 +1,5 @@
+from SmileArgs import SmileArgs
+
+
+smile = SmileArgs()
+smile.run()
